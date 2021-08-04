29c9931f5754b6475f3c3b254fe44023ab349773
Conscat <wgooch2000@gmail.com>
2021-08-04 19:54:41 -0400

    Improved Vec3 struct

--- raytracing.v
+++ raytracing.v
@@ -9,6 +9,7 @@ const (
 )
 
 struct Vec3 {
+mut:
 	data [3]f32
 }
 
@@ -24,68 +25,69 @@ fn (v Vec3) z() f32 {
 	return v.data[2]
 }
 
-fn (va Vec3) + (vb Vec3) Vec3 {
+fn (v Vec3) + (u Vec3) Vec3 {
 	return Vec3{
-		x: va.x() + vb.x()
-		y: va.y() + vb.y()
-		z: va.z() + vb.z()
+		data: [
+			v.x() + u.x(),
+			v.y() + u.y(),
+			v.z() + u.z(),
+		]!
 	}
 }
 
-fn (va Vec3) - (vb Vec3) Vec3 {
+fn (v Vec3) - (u Vec3) Vec3 {
 	return Vec3{
-		x: va.x() - vb.x()
-		y: va.y() - vb.y()
-		z: va.z() - vb.z()
+		data: [
+			v.x() - u.x(),
+			v.y() - u.y(),
+			v.z() - u.z(),
+		]!
 	}
 }
 
-fn (v Vec3) * (t f32) f32 {
-	return Vec3{
-		x: v.x() * t
-		y: v.y() * t
-		z: v.z() * t
-	}
+fn (mut v Vec3) scale(t f32) {
+	v.data = [v.x() * t, v.y() * t, v.z() * t]!
 }
 
-fn (v Vec3) / (t f32) f32 {
-	return Vec3{
-		x: v.x() / t
-		y: v.y() / t
-		z: v.z() / t
-	}
+fn (mut v Vec3) divide(t f32) {
+	v.data = [v.x() / t, v.y() / t, v.z() / t]!
 }
 
 fn (v Vec3) len() f32 {
-   return math.sqrt(v.x()*v.x() + v.y()*v.y() + v.z()*v.z())
+	return math.sqrtf(v.x() * v.x() + v.y() * v.y() + v.z() * v.z())
 }
 
 fn cross(v Vec3, u Vec3) Vec3 {
-   return Vec3 {
-   		 x: u.y() * v.z() - u.z() * v.y(),
-		 y: u.z() * v.x() - u.x() * v.z(),
-		 z: u.x() * v.y() - u.y() * v.x()
-		  }
+	return Vec3{
+		data: [
+			u.y() * v.z() - u.z() * v.y(),
+			u.z() * v.x() - u.x() * v.z(),
+			u.x() * v.y() - u.y() * v.x(),
+		]!
+	}
 }
 
 fn normalize(v Vec3) Vec3 {
-   return v / v.len()
+	mut new_vector := v
+	new_vector.divide(v.len())
+	return new_vector
+}
+
+fn write_color(mut buffer []byte, rgb Vec3) {
+	red := f32(rgb.x()) / (image_width - 1)
+	green := f32(rgb.y()) / (image_height - 1)
+	blue := rgb.z()
+	buffer << byte(255.999 * red)
+	buffer << byte(255.999 * green)
+	buffer << byte(255.999 * blue)
 }
 
 fn main() {
 	mut rgb_buffer := []byte{len: 0, cap: image_width * image_height}
 	for j in 0 .. image_width {
 		for i in 0 .. image_height {
-			red := f32(i) / (image_width - 1)
-			green := f32(j) / (image_height - 1)
-			blue := 0.25
-			ir := byte(255.999 * red)
-			ig := byte(255.999 * green)
-			ib := byte(255.999 * blue)
-			rgb_buffer << ir
-			rgb_buffer << ig
-			rgb_buffer << ib
+			write_color(mut rgb_buffer, Vec3{ data: [f32(i), j, 0.25]! })
 		}
 	}
-	print_rgb_at_point(rgb_buffer, image_width, image_height)
+	kitty.print_rgb_at_point(rgb_buffer, image_width, image_height)
 }
